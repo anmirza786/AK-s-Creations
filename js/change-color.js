8d021d99eaba952ded6f5b1e2d369bdd8f5f3f83
anmirza786 <anmirza360@gmail.com>
2021-08-16 05:04:50 +0100

    Building a Multi color theme.

---
+++ js/change-color.js
@@ -0,0 +1,10 @@
+var btnc = document.getElementById("stylesheet");
+function style1() {
+  btnc.href = "css/style.css";
+}
+function style2() {
+  btnc.href = "css/style2.css";
+}
+function style3() {
+  btnc.href = "css/style3.css";
+}
